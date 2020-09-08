e3e541822ef4c738dc19f3f54ad652e64a9055fb
dms2350 <derianmorales23@gmail.com>
2020-09-07 20:55:08 -0500

    nuevos formularios

--- nacimientos.php
+++ nacimientos.php
@@ -9,5 +9,45 @@
         <?php
         include 'menu.php';
         ?>
+    <div class="container-fluid p-4">
+    <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-3">
+
+         <div class="card card-body">
+            <div class="alert alert-primary" role="alert">
+                <h6 align="center">REGISTRO DE NACIMIENTOS</h6>
+            </div>
+            <form action="" method="POST">
+
+                <select class="form-control" name="padre" required>
+                    <option value="" hidden selected>Seleccione el padre</option>
+                    
+                </select><br>
+
+                <select class="form-control" name="madre" required>
+                    <option value="" hidden selected>Selecciones la madre</option>
+                    
+                </select><br>
+                <select class="form-control" name="sexo" required>
+                    <option value="" hidden selected>Seleccione sexo</option>
+                    
+                    
+                </select><br>
+                <div class="form-group">
+                    <input type="text" onfocus="(this.type='date')"  class="form-control" name="fecha" placeholder="Fecha de nacimiento" required>
+                </div>
+                <select class="form-control" name="lote" required>
+                    <option value="" hidden selected>Seleccione el lote</option>
+                    
+                </select><br>
+                <button type="submit" name="submit" class="btn btn-primary btn-block"> Guardar</button>
+            </form>
+
+        </div>
+    </div>
+    <div class="col-md-3"></div>
+</div>
+    </div>
 </body>
 </html>
--- padrotes.php
+++ padrotes.php
@@ -9,5 +9,41 @@
         <?php
         include 'menu.php';
         ?>
+
+    <div class="container-fluid p-4">
+    <div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-3">
+        <div class="card card-body">
+            <div class="alert alert-primary" role="alert">
+            <h6 align="center">REGISTRO DE PADROTES</h6>
+            </div>
+            <form action="" method="POST">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="color" placeholder="Color" required>
+                </div>
+                <select class="form-control" name="cuerno" required>
+                    <option value="" hidden selected>Seleccione Cuerno</option>
+                    
+                </select><br>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="proce" placeholder="Procedencia" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="raza" placeholder="Raza o Genetica" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" onfocus="(this.type='date')" class="form-control" name="fecha" placeholder="Fecha de compra o cambio" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="precio" placeholder="Precio" required onclick="mostrarToast()">
+                </div>
+                <button class="btn btn-primary btn-block "> Guardar</button>
+            </form>
+        </div>
+    </div>
+    <div class="col-md-3">></div>
+</div>
+    </div>
 </body>
 </html>
